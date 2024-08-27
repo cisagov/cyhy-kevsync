dcce9e34111257dcf722dee6db57bcecdb0a8937
Felddy <markf@geekpad.com>
2024-08-27 13:48:53 -0400

    Update package name and description

--- setup.py
+++ setup.py
@@ -42,14 +42,14 @@ def get_version(version_file):
 
 
 setup(
-    name="example",
+    name="cyhy-kevsync",
     # Versions should comply with PEP440
-    version=get_version("src/example/_version.py"),
-    description="Example Python library",
+    version=get_version("src/cyhy_kevsync/_version.py"),
+    description="Cyber Hygiene known exploited vulnerability (KEV) synchronization tool",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    # Landing page for CISA's cybersecurity mission
-    url="https://www.cisa.gov/cybersecurity",
+    # Landing page for CISA's Known Exploited Vulnerabilities Catalog
+    url="https://www.cisa.gov/known-exploited-vulnerabilities-catalog",
     # Additional URLs for this project per
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#project-urls
     project_urls={
@@ -57,8 +57,8 @@ setup(
         "Tracker": "https://github.com/cisagov/cyhy-kevsync/issues",
     },
     # Author details
-    author="Cybersecurity and Infrastructure Security Agency",
-    author_email="github@cisa.dhs.gov",
+    author="Mark Feldhousen",
+    author_email="mark.feldhousen@cisa.dhs.gov",
     license="License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -75,23 +75,24 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     # What does your project relate to?
-    keywords="skeleton",
+    keywords="cyhy kev sync",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={"example": ["data/*.txt"]},
+    package_data={},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
-    include_package_data=True,
-    install_requires=["docopt", "schema", "setuptools >= 24.2.0"],
+    install_requires=[
+        "cyhy-config @ git+https://github.com/cisagov/cyhy-config.git@first-commits",
+        "cyhy-db @ git+https://github.com/cisagov/cyhy-db.git@first-commits",
+        "rich",
+        "schema",
+        "setuptools >= 24.2.0",
+    ],
     extras_require={
         "test": [
             "coverage",
@@ -108,5 +109,5 @@ setup(
         ]
     },
     # Conveniently allows one to run the CLI tool as `example`
-    entry_points={"console_scripts": ["example = example.example:main"]},
+    entry_points={"console_scripts": ["cyhy-kevsync = cyhy_kevsync.cyhy_kevsync:main"]},
 )
